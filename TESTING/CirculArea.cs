d23768b9c020334d613f7b734e9bb46e5f63b67d
user <user@DESKTOP-M1319SC>
2023-06-08 10:08:44 +0500

    Добавьте файлы проекта.

---
+++ TESTING/CirculArea.cs
@@ -0,0 +1,19 @@
+﻿namespace TESTING
+{
+    public class Circul
+    {
+        public double Radius { get; }
+        public Circul(double radius)
+        {
+            if(radius < 1e-7)
+            {
+                throw new ArgumentException("Radius count Error", nameof(radius));
+            }
+            Radius = radius;
+        }
+        public double CalcSquare()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ TESTING/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTING
+{
+    public class Rectangle
+    {
+        private double p;
+        public double a { get; }
+        public double b { get; }
+        public double c { get; }
+        public Rectangle(double a, double b, double c)
+        {
+            if (a < 1e-7) throw new ArgumentException("Count Eror", nameof(a));
+            if (b < 1e-7) throw new ArgumentException("Count Eror", nameof(b));
+            if (c < 1e-7) throw new ArgumentException("Count Eror", nameof(c));
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double CalcSquare()
+        {
+            try
+            {
+                p = (a + b + c) / 2;
+            }
+            catch (Exception ex)
+            {
+
+                throw new ArgumentException("parameters is not valid", nameof(a));
+                throw new ArgumentException("parameters is not valid", nameof(b));
+                throw new ArgumentException("parameters is not valid", nameof(c));
+            }
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
